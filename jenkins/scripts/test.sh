eaec532e8a6245c46ac4f58a64d1e4b1fc8740a1
bytebaron <bytebaron2000@gmail.com>
2020-03-31 18:16:51 +0100

    another change.

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -18,4 +18,4 @@ echo 'application renders satisfactorily. This command actually invokes the test
 echo 'runner Jest (https://facebook.github.io/jest/).'
 set -x
 pwd
-npm test --prefix /home/bytebaron/development/organizer/client
+npm test 
