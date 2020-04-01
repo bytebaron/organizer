4c1bfbb49aa954859a97d1df09303236baaeeae8
bytebaron <bytebaron2000@gmail.com>
2020-04-01 12:30:48 +0100

    jknckjzdnjv.

--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -1,4 +1,4 @@
-import React, from 'react';
+import React from 'react';
 //import { useTable } from 'react-table'
 //import { DndProvider, useDrag, useDrop } from 'react-dnd'
 //import HTML5Backend from 'react-dnd-html5-backend'
